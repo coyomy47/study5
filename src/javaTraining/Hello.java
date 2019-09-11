2cdf6820a0d8cff8ef0cab5374f8b585321ce1f8
coyom <coyom@DESKTOP-97KN9UF>
2019-09-11 10:27:26 +0900

    백준 2562번 최댓값 구하기 문제 배열 만들기 성공

--- src/javaTraining/Hello.java
+++ src/javaTraining/Hello.java
@@ -2,7 +2,12 @@ package javaTraining;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("세상아 반갑다!");
-		System.out.println("hello world!!");
+		System.out.println("\\     /\\");
+		System.out.println(" )   ( ')"); 
+		System.out.println("(  /  )"); 
+		System.out.println(" \\(__)|"); 
+		
+			
+		
 	}
 }
