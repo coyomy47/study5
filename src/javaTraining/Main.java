2f60e36008b86aeabefaf27fd544cc16d8ff8d5a
coyom <coyom@DESKTOP-97KN9UF>
2019-09-15 19:49:24 +0900

    index값을 1로 바꾸었더니 해결되었습니다.

--- src/javaTraining/Main.java
+++ src/javaTraining/Main.java
@@ -8,17 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("시작!");
 		Scanner scan = new Scanner(System.in);// 스캐너 함수를 부른다.
 		int [] array = new int[9];// 9개짜리 정수가들어가는 배열을 만든다.			
-		
 
 		for(int i=0;i<array.length;i++) {			// 배열에 9개의 수를 입력받아 넣습니다.
 			array[i]=scan.nextInt();	
 		}
 		
 		int max = array[0];			// 기준이 될 첫번 째 값을 max에 넣음 
-		int index = 0;			// 최대값의 위치를 알아낼 변수 생성
+		int index = 1;			// 최대값의 위치를 알아낼 변수 생성
 		
 		for(int i=1;i<array.length;i++){
 			if(array[i] > max) 			// max는 array[0]이므로, max보다 큰 값이 있으면 그 값이 max가 됩니다.
