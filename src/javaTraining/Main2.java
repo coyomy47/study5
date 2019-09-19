a2a427dd222106da0b185e6676c25eee255440a9
coyom <coyom@DESKTOP-97KN9UF>
2019-09-19 10:56:42 +0900

    문자열 단순비교로 바꿈

--- src/javaTraining/Main2.java
+++ src/javaTraining/Main2.java
@@ -8,27 +8,21 @@ public class Main2 {
 		// TODO Auto-generated method stub
 		
 		Scanner scan = new Scanner(System.in);	//스캐너 함수를 부른다.
-        int[] array= new int[8];	//8개짜리의 정수가 들어가는 배열을만든다.
+        String input=scan.nextLine(); 	//문자열을 받는다.
         
-        for(int i=0;i<array.length;i++) {	// 배열에 8개의 수를 입력받아 넣습니다.
-        	array[i] = scan.nextInt();
-        }
-        
-        scan.close();
-        
+        String A="1 2 3 4 5 6 7 8";		//문자열 "1,2,3,4,5,6,7,8"생성
+        String D="8 7 6 5 4 3 2 1";		//문자열 "8,7,6,5,4,3,2,1"생성
         String output=new String();		//문자열 생성
     
-        for(int i=0;i<array.length-1;i++){		//반복문 시작
-            if(array[i]==array[i+1]-1){		//if문시작,	배열의 값에서 1을 뺐을때 순서대로 배열의 값이 같아야 한다.
-                output="ascending";		//순서대로 입력되었을시 "ascending"출력
-            }else if(array[i]==array[i+1]+1){	//배열의 값에서 1을 더했을때 순서대로 배열의 값이 같아야 한다.
-                output="descending";	//순서대로 입력 되었을시 "descending"출력
-            } else {
-                output="mixed";		// 둘다 아닐시 "mixed"출력
-                break;					//if문 중단
-            }
-        }		//반복문 종료
-        
+        if(input.equals(A)){		//if문시작, 받은 문자열 과 A의 문자열이 같으면	
+            output="ascending";		//"ascending"
+        }else if(input.equals(D)){	//받은문자열 이같으면
+        	output="descending";	// "descending"출력
+        } else {
+            output="mixed";		// 둘다 아닐시 "mixed"출력
+        }
+
+        scan.close();
         System.out.println(output);	//문자열출력
       
 	}
