8bfa3829ee17929279db5fc2b299208cd938bfd3
coyom <coyom@DESKTOP-97KN9UF>
2019-09-19 10:12:01 +0900

    사소한 수정

--- src/javaTraining/Main2.java
+++ src/javaTraining/Main2.java
@@ -6,15 +6,18 @@ public class Main2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner scan = new Scanner(System.in);	//스캐너 함수를 부른다.
         int[] array= new int[8];	//8개짜리의 정수가 들어가는 배열을만든다.
         
-        for (int i=0;i<array.length;i++) {	// 배열에 8개의 수를 입력받아 넣습니다.
+        for(int i=0;i<array.length;i++) {	// 배열에 8개의 수를 입력받아 넣습니다.
         	array[i] = scan.nextInt();
         }
+        
         scan.close();
-        String output=new String();		//문자열 생성
-        for (int i=0;i<array.length-1;i++){		//반복문 시작
+        String output="";		//문자열 생성
+    
+        for(int i=0;i<array.length-1;i++){		//반복문 시작
             if(array[i]==array[i+1]-1){		//if문시작,	배열의 값에서 1을 뺐을때 순서대로 배열의 값이 같아야 한다.
                 output="ascending";		//순서대로 입력되었을시 "ascending"출력
             }else if(array[i]==array[i+1]+1){	//배열의 값에서 1을 더했을때 순서대로 배열의 값이 같아야 한다.
@@ -24,6 +27,7 @@ public class Main2 {
                 break;					//if문 중단
             }
         }		//반복문 종료
+        
         System.out.println(output);	//문자열출력
       
 	}
