6390baea56064003d9e53e294fc195943c46b004
coyom <coyom@DESKTOP-97KN9UF>
2019-09-19 09:53:27 +0900

    반복문 생성 완료

--- src/javaTraining/Main2.java
+++ src/javaTraining/Main2.java
@@ -15,12 +15,17 @@ public class Main2 {
         
         String output;
         for (int i=0;i<array.length-1;i++){		//반복문 시작
-            if(array[i]==array[i+1]-1){		//배열의 값에서 1을 뺐을때 순서대로 배열의 값이 같아야 한다.
+            if(array[i]==array[i+1]-1){		//if문시작,	배열의 값에서 1을 뺐을때 순서대로 배열의 값이 같아야 한다.
                 output="ascending";		//순서대로 입력되었을시 "ascending"출력
-            
+            }else if(array[i]==array[i+1]+1){	//배열의 값에서 1을 더했을때 순서대로 배열의 값이 같아야 한다.
+                output ="descending";	//순서대로 입력 되었을시 "descending"출력
+            } else {
+                output = "mixed";		// 둘다 아닐시 "mixed"출력
+                break;					//if문 중단
+            }
+        }		//반복문 종료
+        System.out.println(output);
       
 	}
 
-
-	}
 }
